ef9812b7463e34fbb849d4eebd1c847bc2834a51
Nafi Robayat <nafi@live.com>
2025-12-02 20:13:42 +1100

    Added initial day2 data and functions

---
+++ src/days/day2.rs
@@ -0,0 +1,13 @@
+use aoc_utils::get_day_lines;
+
+pub fn part1() {
+    // let file_lines: Vec<String> = get_day_lines("day2");
+    // let mut answer: i128 = 0;
+    // println!("Day2 Part 1 answer: {:?}", answer);
+}
+
+pub fn part2() {
+    // let file_lines: Vec<String> = get_day_lines("day2");
+    // let mut answer: i128 = 0;
+    // println!("Day2 Part 2 answer: {:?}", count);
+}
